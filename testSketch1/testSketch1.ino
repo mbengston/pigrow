ad8bf8c768503836819c6a9e092e5c8289b9b4c7
Mitchell Bengston <bengstonm@gmail.com>
2016-09-11 08:58:23 -0700

    mcp3008 arduino

--- testSketch1/testSketch1.ino
+++ testSketch1/testSketch1.ino
@@ -1,9 +1,21 @@
 //#include <stdlib.h>
 #include <dht.h> //dht11 sensor library
 #define DELA 1000  // Time between message sends.
+
+#include <MCP3008.h>
+
+
+// define pin connections
+#define CS_PIN 12
+#define CLOCK_PIN 9
+#define MOSI_PIN 11
+#define MISO_PIN 10
+
+// put pins inside MCP3008 constructor
+MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 dht DHT;
 #define DHT11_PIN 7
-int MOISTURE_SENSOR_PIN = A1;
+// int MOISTURE_SENSOR_PIN = A1;
 int moistureValue = 0;
 
 void setup() {
@@ -25,9 +37,16 @@ void loop()
     }
     else if (ch == '3')
     {
-      moistureValue = analogRead(MOISTURE_SENSOR_PIN);
+      moistureValue = adc.readADC(0);
       Serial.println(moistureValue);
+
+//      iterate thru all channels
+//      for (int i=0; i<8; i++) {
+//        moistureValue = adc.readADC(i);
+//        Serial.println(moistureValue);
+//      }
     }
   }
   delay(DELA);
 }
+
